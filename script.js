acb2046be91440b40cb34755c17706c0c8261b48
Neceu <55215081+Neceu@users.noreply.github.com>
2019-09-11 20:45:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-// Where is the circle
-let x, y;
+var x = 200;
+var y = 200;
 
-function setup() {
-  createCanvas(720, 400);
-  // Starts in the middle
-  x = width / 2;
-  y = height;
-}
 
-function draw() {
-  background(200);
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+  fill(0);
+  ellipse(x,y,50,50);
   
-  // Draw a circle
-  stroke(50);
-  fill(100);
-  ellipse(x, y, 24, 24);
-  
-  // Jiggling randomly on the horizontal axis
-  x = x + random(-1, 1);
-  // Moving up at a constant speed
-  y = y - 1;
-  
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
+  //x = x + 1;
+ 
+    
+  if (x >= 400){
+   x = 0; 
   }
+  if (keyIsDown(UP_ARROW)) {
+    y = y - 10;
+  } else if (keyIsDown(DOWN_ARROW)) {
+   y = y + 10;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    x = x - 5;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    x = x + 5;
+  }
+ 
 }
